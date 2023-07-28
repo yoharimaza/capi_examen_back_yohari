f77603bec00f3bd8f929baaa5bb3eb3433b7d45e
Yohari Maza <yoharimaza@gmail.com>
2023-07-28 17:12:43 -0600

    added env file

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,9 +8,6 @@ use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
-
-
     public function getUserDomicilios(){
         $sql = "SELECT *, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), users.fecha_nacimiento)), '%Y') + 0 AS age FROM users, user_domicilios WHERE users.id = user_domicilios.user_id";
         $users = DB::select($sql);
